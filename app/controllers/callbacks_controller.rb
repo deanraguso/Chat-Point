92080f72058370c1225736f0ad60ca754418a74c
dean <deanraguso@gmail.com>
2021-06-08 12:26:02 +1000

    Created: Google omni-auth now works, name doesn't need to be unique anymore!

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,4 +3,14 @@ class CallbacksController < ApplicationController
         @user = User.from_omniauth(request.env["omniauth.auth"])
         sign_in_and_redirect @user
     end
+
+    def google_oauth2
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        sign_in_and_redirect @user
+    end
+
+    def failure
+        redirect_to root_path
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable
 
-  validates :name, uniqueness: true, presence: true
+  validates :name, presence: true
   has_many :messages, dependent: :destroy
   has_and_belongs_to_many :rooms
 
