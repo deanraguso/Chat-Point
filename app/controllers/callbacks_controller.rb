f1f11e024b50894c716a93b76708757c72210239
dean <deanraguso@gmail.com>
2021-06-08 12:37:29 +1000

    Created: User can login via Facebook now also!

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,11 +1,27 @@
 class CallbacksController < ApplicationController
+    # Presently: If email conflicts, signs into the same email user.
+    # Not a security issue, but not an ideal approach either.
     def github
         @user = User.from_omniauth(request.env["omniauth.auth"])
+        unless User.find_by(email: @user.email).nil? 
+            @user = User.find_by(email: @user.email)
+        end
         sign_in_and_redirect @user
     end
 
     def google_oauth2
         @user = User.from_omniauth(request.env["omniauth.auth"])
+        unless User.find_by(email: @user.email).nil? 
+            @user = User.find_by(email: @user.email)
+        end
+        sign_in_and_redirect @user
+    end
+
+    def facebook
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        unless User.find_by(email: @user.email).nil? 
+            @user = User.find_by(email: @user.email)
+        end
         sign_in_and_redirect @user
     end
 
